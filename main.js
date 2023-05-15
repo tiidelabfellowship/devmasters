4e97950391f115cfce00cc5f766869cf70c574e2
Daniel Omoanon <drimbignation@gmail.com>
2023-05-15 07:21:03 -0700

    carousel and login

--- main.js
+++ main.js
@@ -42,3 +42,58 @@ const sidebarDietHandle = () => {
     }
  }
 
+ // Script for carousel by leo and daniel
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// login javascript starts here
+
+const forms = document.querySelector(".formsy"),
+       pwShowHide = document.querySelectorAll(".eye-icon"),
+       links = document.querySelectorAll(".link");
+
+// console.log(forms, pwShowHide,links )
+pwShowHide.forEach(eyeIcon =>{
+    eyeIcon.addEventListener("click", () =>{
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        // console.log(pwFields)
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide","bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-hide","bx-hide");
+        })
+    })
+   
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();// prevent form SubmitEvent
+        forms.classList.toggle("show-signup");
+    })
+})
+
+// login javscript ends here
+
+
