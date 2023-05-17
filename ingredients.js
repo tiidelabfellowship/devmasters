93dae0bd10629275182a056305414bbd5da538dd
Nwafor Stellamaris <wendumaris@gmail.com>
2023-05-17 15:24:21 +0100

    added ingredients page

---
+++ ingredients.js
@@ -0,0 +1,206 @@
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+searchBtn.addEventListener("click", () => {
+  let userInp = document.getElementById("user-inp").value;
+  if (userInp.length == 0) {
+    result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+  } else {
+    fetch(url + userInp)
+      .then((response) => response.json())
+      .then((data) => {
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInstructions);
+        let count = 1;
+        let ingredients = [];
+        for (let i in myMeal) {
+          let ingredient = "";
+          let measure = "";
+          if (i.startsWith("strIngredient") && myMeal[i]) {
+            ingredient = myMeal[i];
+            measure = myMeal[`strMeasure` + count];
+            count += 1;
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+        }
+        console.log(ingredients);
+        result.innerHTML = `
+    <img src=${myMeal.strMealThumb}>
+    <div class="details">
+        <h2>${myMeal.strMeal}</h2>
+        <h4>${myMeal.strArea}</h4>
+    </div>
+    <div id="ingredient-con"></div>
+    <div id="recipe">
+        <button id="hide-recipe">X</button>
+        <pre id="instructions">${myMeal.strInstructions}</pre>
+    </div>
+    <button id="show-recipe">View Recipe</button>
+    `;
+        let ingredientCon = document.getElementById("ingredient-con");
+        let parent = document.createElement("ul");
+        let recipe = document.getElementById("recipe");
+        let hideRecipe = document.getElementById("hide-recipe");
+        let showRecipe = document.getElementById("show-recipe");
+        ingredients.forEach((i) => {
+          let child = document.createElement("li");
+          child.innerText = i;
+          parent.appendChild(child);
+          ingredientCon.appendChild(parent);
+        });
+        hideRecipe.addEventListener("click", () => {
+          recipe.style.display = "none";
+        });
+        showRecipe.addEventListener("click", () => {
+          recipe.style.display = "block";
+        });
+      })
+      .catch(() => {
+        result.innerHTML = `<h3>Invalid Input</h3>`;
+      });
+  }
+});
+
+    var team_slider = new Swiper(".team-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1.2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+    });
+
+    jQuery(".filters").on("click", function () {
+        jQuery("#menu-dish").removeClass("bydefault_show");
+    });
+    $(function () {
+        var filterList = {
+            init: function () {
+                $("#menu-dish").mixItUp({
+                    selectors: {
+                        target: ".dish-box-wp",
+                        filter: ".filter",
+                    },
+                    animation: {
+                        effects: "fade",
+                        easing: "ease-in-out",
+                    },
+                    load: {
+                        filter: ".all, .breakfast, .lunch, .dinner",
+                    },
+                });
+            },
+        };
+        filterList.init();
+    });
+
+    jQuery(".menu-toggle").click(function () {
+        jQuery(".main-navigation").toggleClass("toggled");
+    });
+
+    jQuery(".header-menu ul li a").click(function () {
+        jQuery(".main-navigation").removeClass("toggled");
+    });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    var elementFirst = document.querySelector('.site-header');
+    ScrollTrigger.create({
+        trigger: "body",
+        start: "30px top",
+        end: "bottom bottom",
+
+        onEnter: () => myFunction(),
+        onLeaveBack: () => myFunction(),
+    });
+
+    function myFunction() {
+        elementFirst.classList.toggle('sticky_head');
+    }
+
+    var scene = $(".js-parallax-scene").get(0);
+    var parallaxInstance = new Parallax(scene);
+
+
+
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
+
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
+    let animation;
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].index = i;
+        targets[i].addEventListener("click", moveBar);
+    }
+
+    // initial position on first === All
+    gsap.set(".filter-active", {
+        x: targets[0].offsetLeft,
+        width: targets[0].offsetWidth
+    });
+
+    function moveBar() {
+        if (this.index != activeTab) {
+            if (animation && animation.isActive()) {
+                animation.progress(1);
+            }
+            animation = gsap.timeline({
+                defaults: {
+                    duration: 0.4
+                }
+            });
+            old = activeTab;
+            activeTab = this.index;
+            animation.to(".filter-active", {
+                x: targets[activeTab].offsetLeft,
+                width: targets[activeTab].offsetWidth
+            });
+
+            animation.to(targets[old], {
+                color: "#0d0d25",
+                ease: "none"
+            }, 0);
+            animation.to(targets[activeTab], {
+                color: "#fff",
+                ease: "none"
+            }, 0);
+
+        }
+
+    }
+});
